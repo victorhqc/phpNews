6900fc503e063cfcecf68db437afd63a780b3063
victorhqc <victorhqc@gmail.com>
2013-07-28 14:52:02 -0600

    Added support for automatic language detection

--- js/main.js
+++ js/main.js
@@ -5,15 +5,38 @@ var App;
 	var Init = function(){
 		//Gets the initial modules
 		var I = this;
+		var lang = this.browserLanguage();
 		new Vi({url:'data.json', response:'json'}).server(function(r){
-			var j = {name: 'My App', modules:r.modules, div:'#content'};
+			var j = {name: 'My App', modules:r.modules, div:'#content', currentLang: lang};
 			App = new AppSystem(j);
+			App._data = r;
 			App.init(function(){
 				//Checks if the user is logged in.
 				I.checkCredentials();
 			});
 		});
 	}
+
+	Init.prototype.browserLanguage = function() {
+		var lang = navigator.language || navigator.userLanguage;
+		lang = lang.match(/([a-z]+)/gi);
+		if(lang !== null){
+			lang = lang[0];
+		}
+
+		var l = '';
+		switch(lang){
+			case 'es':
+				l = lang;
+			break;
+			default:
+			case 'en':
+				l = lang;
+			break;
+		}
+
+		return l;
+	};
 
 	Init.prototype.checkCredentials = function() {
 		new Vi({url:'members.php', response: 'json'}).server(function(r){
